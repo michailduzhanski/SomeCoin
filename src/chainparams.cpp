3ee999c82ef80f39472d85fde09bd6ef380a1fde
matas <anmatasov@gmail.com>
2020-02-03 23:48:41 +0200

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -146,16 +146,32 @@ public:
 		vSeeds.push_back(CDNSSeedData("dns.arnak.org", "dnsseed.arnak.org")); // Arnak
 		
         // guarantees the first 2 characters, when base58 encoded, are "v1"
-        base58Prefixes[PUBKEY_ADDRESS]     = {0x1D,0xD8};
-        // guarantees the first 2 characters, when base58 encoded, are "v3"
-        base58Prefixes[SCRIPT_ADDRESS]     = {0x1D,0xDE};
+        // base58Prefixes[PUBKEY_ADDRESS]     = {0x1D,0xD8};
+        // // guarantees the first 2 characters, when base58 encoded, are "v3"
+        // base58Prefixes[SCRIPT_ADDRESS]     = {0x1D,0xDE};
+        // // the first character, when base58 encoded, is "5" or "K" or "L" (as in Bitcoin)
+        // base58Prefixes[SECRET_KEY]         = {0x80};
+        // // do not rely on these BIP32 prefixes; they are not specified and may change
+        // base58Prefixes[EXT_PUBLIC_KEY]     = {0x04,0x88,0xB2,0x1E};
+        // base58Prefixes[EXT_SECRET_KEY]     = {0x04,0x88,0xAD,0xE4};
+        // // guarantees the first 2 characters, when base58 encoded, are "hd"
+        // base58Prefixes[ZCPAYMENT_ADDRRESS] = {0x16,0x9A};
+        // base58Prefixes[ZCVIEWING_KEY]      = {0xA8,0xAB,0xD3};
+        // // guarantees the first 2 characters, when base58 encoded, are "SK"
+        // base58Prefixes[ZCSPENDING_KEY]     = {0xAB,0x36};
+		
+		// guarantees the first 2 characters, when base58 encoded, are "t1"
+        base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0xB8};
+        // guarantees the first 2 characters, when base58 encoded, are "t3"
+        base58Prefixes[SCRIPT_ADDRESS]     = {0x1C,0xBD};
         // the first character, when base58 encoded, is "5" or "K" or "L" (as in Bitcoin)
         base58Prefixes[SECRET_KEY]         = {0x80};
         // do not rely on these BIP32 prefixes; they are not specified and may change
         base58Prefixes[EXT_PUBLIC_KEY]     = {0x04,0x88,0xB2,0x1E};
         base58Prefixes[EXT_SECRET_KEY]     = {0x04,0x88,0xAD,0xE4};
-        // guarantees the first 2 characters, when base58 encoded, are "hd"
+        // guarantees the first 2 characters, when base58 encoded, are "zc"
         base58Prefixes[ZCPAYMENT_ADDRRESS] = {0x16,0x9A};
+        // guarantees the first 4 characters, when base58 encoded, are "ZiVK"
         base58Prefixes[ZCVIEWING_KEY]      = {0xA8,0xAB,0xD3};
         // guarantees the first 2 characters, when base58 encoded, are "SK"
         base58Prefixes[ZCSPENDING_KEY]     = {0xAB,0x36};
